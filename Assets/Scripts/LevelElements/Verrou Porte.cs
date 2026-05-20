2d1bb41007dc514508719c12910a2e36e10a6c1d
MaxDonder <macsven.donato-derouen@mmibordeaux.com>
2026-05-20 16:24:40 +0200

    List Verrou

---
+++ Assets/Scripts/LevelElements/Verrou Porte.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class VerrouPorte : MonoBehaviour
+{
+    
+    public List<Levier> leviers;
+    private string 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    for each (Levier levier in leviers) {
+        if (!levier.isActivated) {
+            return;
+        }
+    }
+}
